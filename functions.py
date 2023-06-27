d265bb098c234fe7a53aa7ec94077302fadb104c
Pielau92 <106060352+Pielau92@users.noreply.github.com>
2023-06-27 13:27:29 +0200

    Complete documentation
    Completed documentation, also added todos and minor cleanup.

--- functions.py
+++ functions.py
@@ -1,43 +1,42 @@
 import re
 
 
-def replace_param(match, parameters):
+def replace_text(match, parameters):
     """
 
     Parameters
     ----------
-    match :
-    parameters :
-
-    Returns
-    -------
-
+    match : re.Match
+        Match from the find_and_replace_text method.
+    parameters : pandas.Series
+        Series with parameter values to be replaced, the parameter name has to be in the index name.
     """
     for name, value in parameters.items():
         if match.group(1).casefold() == '@' + name.casefold():  # find, case-insensitive
             return match.group(1)[1:] + '=' + str(value)
 
 
-def find_and_replace_param(path_file, pattern, parameters):
-    """
+def find_and_replace_text(path_file, pattern, parameters):
+    """Find and replace text within txt file.
+
+    Find text within a txt file which matches a certain pattern and replace it.
 
     Parameters
     ----------
-    path_file : path of the .txt file
-    pattern :
-    parameters :
-
-    Returns
-    -------
-
+    path_file : str
+        Path of the .txt file
+    pattern : str
+        Pattern which marks parameters to be replaced
+    parameters : pandas.Series
+        Series with parameter values to be replaced, the parameter name has to be in the index name.
     """
 
     # read file
     with open(path_file, 'r') as file:
         text = file.read()
 
-        # find, then replace parameter values
-        text = re.sub(pattern=pattern, repl=lambda match: replace_param(match, parameters), string=text)
+        # find, then replace text values
+        text = re.sub(pattern=pattern, repl=lambda match: replace_text(match, parameters), string=text)
 
     # overwrite file
     with open(path_file, 'w') as file:
