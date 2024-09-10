764a162c1be9b06f3e676010dc0cfb466fe04b50
Pielau92 <106060352+Pielau92@users.noreply.github.com>
2024-09-10 13:57:10 +0200

    Simplify convert_16_48

--- functions_mpc.py
+++ functions_mpc.py
@@ -12,30 +12,17 @@ from statistics import mean
 def convert_16_48(Q_heat_s, n):
     Q_heat = np.zeros(n)
 
-    for i in range(n):
-        if i < 6:
-            Q_heat[i] = Q_heat_s[i]
-        if 6 <= i < 8:
-            Q_heat[i] = Q_heat_s[6]
-        if 8 <= i < 10:
-            Q_heat[i] = Q_heat_s[7]
-        if 10 <= i < 12:
-            Q_heat[i] = Q_heat_s[8]
-        if 12 <= i < 15:
-            Q_heat[i] = Q_heat_s[9]
-        if 15 <= i < 18:
-            Q_heat[i] = Q_heat_s[10]
-        if 18 <= i < 21:
-            Q_heat[i] = Q_heat_s[11]
-        if 21 <= i < 24:
-            Q_heat[i] = Q_heat_s[12]
-        if 24 <= i < 30:
-            Q_heat[i] = Q_heat_s[13]
-        if 30 <= i < 36:
-            Q_heat[i] = Q_heat_s[14]
-        if 36 <= i < 48:
-            Q_heat[i] = Q_heat_s[15]
-        # print(i, Q_heat[i], Q_heat_s[i])
+    Q_heat[0:6] = Q_heat_s[0:6]
+    Q_heat[6:8] = Q_heat_s[6]
+    Q_heat[8:10] = Q_heat_s[7]
+    Q_heat[10:12] = Q_heat_s[8]
+    Q_heat[12:15] = Q_heat_s[9]
+    Q_heat[15:18] = Q_heat_s[10]
+    Q_heat[18:21] = Q_heat_s[11]
+    Q_heat[21:24] = Q_heat_s[12]
+    Q_heat[24:30] = Q_heat_s[13]
+    Q_heat[30:36] = Q_heat_s[14]
+    Q_heat[36:48] = Q_heat_s[15]
 
     return Q_heat
 
