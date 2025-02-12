1a05d427c5f9c9ee20af58a6ddcc34b328218491
Pielau92 <106060352+Pielau92@users.noreply.github.com>
2025-02-12 13:06:51 +0100

    Remove obsolete options in build_executable
    Removed obsolete options passed to pyinstaller inside of build_executable.bat.

--- scripts/build_executable.bat
+++ scripts/build_executable.bat
@@ -21,7 +21,8 @@ set MAIN_PATH=%PROJECT_PATH%\%PROJECT_NAME%\main.py
 rem call PyInstaller to create executable
 echo Creating .exe file with PyInstaller...
 cd %PROJECT_PATH%
-%PYINSTALLER_PATH% %MAIN_PATH% --clean --onefile --hidden-import=openpyxl.cell._writer --hidden-import=win32timezone --hidden-import=pyexpat --collect-all xml.parsers.expat
+%PYINSTALLER_PATH% %MAIN_PATH% --clean --onefile --add-binary "C:/Users/pierre/miniconda3/DLLs/pyexpat.pyd;dlls"
+rem --hidden-import=openpyxl.cell._writer --hidden-import=win32timezone --hidden-import=tkinter --hidden-import=pyexpat  --collect-all xml.parsers.expat
 
 if %errorlevel% neq 0 (
     echo An error occured while creating executable .exe-file
