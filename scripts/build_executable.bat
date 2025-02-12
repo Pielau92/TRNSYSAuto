11f226e5ae04ef1dae241a166a7aaf60e435ca49
Pielau92 <106060352+Pielau92@users.noreply.github.com>
2025-02-12 14:55:21 +0100

    Resolve missing dll error
    Resolved missing dll error completely, by adding path to dlls manually. Deleted  build_executable_from_spec and main.spec as they are obsolete.

--- scripts/build_executable.bat
+++ scripts/build_executable.bat
@@ -21,8 +21,9 @@ set MAIN_PATH=%PROJECT_PATH%\%PROJECT_NAME%\main.py
 cd %PROJECT_PATH%
 
 echo Creating .exe file with PyInstaller...
-%PYINSTALLER_PATH% %MAIN_PATH% --clean --onefile --add-binary "C:/Users/pierre/miniconda3/DLLs/pyexpat.pyd;dlls"
-rem --hidden-import=openpyxl.cell._writer --hidden-import=win32timezone --hidden-import=tkinter --hidden-import=pyexpat  --collect-all xml.parsers.expat
+%PYINSTALLER_PATH% %MAIN_PATH% --clean --onefile --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/DLLs/pyexpat.pyd;dlls" --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/libexpat.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/libcrypto-3-x64.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/libssl-3-x64.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/liblzma.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/LIBBZ2.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/sqlite3.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/tk86t.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/tcl86t.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/ffi.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/libmpdec-4.dll;."
+
+rem --collect-all xml.parsers.expat --hidden-import=pyexpat --collect-all pyexpat --hidden-import=openpyxl.cell._writer --hidden-import=win32timezone --hidden-import=tkinter 
 
 echo Copying executable into %PROJECT_PATH%
 xcopy "%PROJECT_PATH%\dist\main.exe" . /y
