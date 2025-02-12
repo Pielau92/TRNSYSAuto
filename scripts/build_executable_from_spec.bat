9f3a00c434f00725bd077ebbbdfb3bcfae32fae3
Pielau92 <106060352+Pielau92@users.noreply.github.com>
2025-02-12 13:12:47 +0100

    Add build_executable_from_spec.bat
    Added batch file, which creates an executable file from the main.spec file. Otherwise (when using build_executable)  pyexpat is not integrated in the exe file and an error occurs.

---
+++ scripts/build_executable_from_spec.bat
@@ -0,0 +1,32 @@
+@echo off
+setlocal
+
+rem -------------------- BEFORE YOU START --------------------
+
+rem Are you using miniconda3 or anaconda3?
+set CONDA=miniconda3
+
+rem Name of your virtual environment
+set VENV_NAME=TRNSYSAuto
+
+rem Name of spec file
+set SPEC_NAME=main.spec
+
+rem ----------------------------------------------------------
+
+rem Path variables
+set VENV_PATH=%USERPROFILE%\%CONDA%\envs\%VENV_NAME%
+set PYINSTALLER_PATH=%VENV_PATH%\Scripts\pyinstaller.exe
+
+rem call PyInstaller to create executable from spec file
+echo Creating .exe file with PyInstaller...
+%PYINSTALLER_PATH% %SPEC_NAME%
+
+if %errorlevel% neq 0 (
+    echo An error occured while creating executable .exe-file
+    exit /b %errorlevel%
+)
+
+echo Executable created successfully!
+endlocal
+pause
