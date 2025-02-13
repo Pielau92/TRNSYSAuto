92213653e36ddc71038f077e677e6e8e42536f5a
Pielau92 <106060352+Pielau92@users.noreply.github.com>
2025-02-13 14:23:10 +0100

    Update create_installer.bat
    Copy settings.ini into dist directory before creating installer.

--- scripts/create_installer.bat
+++ scripts/create_installer.bat
@@ -15,14 +15,16 @@ rem Path variables
 for %%i in ("%CD%\..") do set PROJECT_PATH=%%~fi
 set INNO_SETUP_EXE_PATH="C:\Program Files (x86)\Inno Setup 6\ISCC.exe"
 set INNO_SETUP_SCRIPT_PATH="%CD%\%INNO_SETUP_SCRIPT_NAME%"
+set PROJECT_PATH=%USERPROFILE%\PycharmProjects\%PROJECT_NAME%
 
 rem Build executable .exe-file
 call build_executable.bat nopause
 
 rem update MPC modules in dist\assets\mpc_code directory
-xcopy "%USERPROFILE%\PycharmProjects\%PROJECT_NAME%\assets\mpc_code\main_mpc.py" "%USERPROFILE%\PycharmProjects\%PROJECT_NAME%\dist\assets\mpc_code\" /y
-xcopy "%USERPROFILE%\PycharmProjects\%PROJECT_NAME%\assets\mpc_code\MPCModule.py" "%USERPROFILE%\PycharmProjects\%PROJECT_NAME%\dist\assets\mpc_code\" /y
-xcopy "%USERPROFILE%\PycharmProjects\%PROJECT_NAME%\assets\mpc_code\settingsMPC.ini" "%USERPROFILE%\PycharmProjects\%PROJECT_NAME%\dist\assets\mpc_code\" /y
+xcopy "%PROJECT_PATH%\assets\mpc_code\main_mpc.py" "%PROJECT_PATH%\dist\assets\mpc_code\" /y
+xcopy "%PROJECT_PATH%\assets\mpc_code\MPCModule.py" "%PROJECT_PATH%\dist\assets\mpc_code\" /y
+xcopy "%PROJECT_PATH%\assets\mpc_code\settingsMPC.ini" "%PROJECT_PATH%\dist\assets\mpc_code\" /y
+xcopy "%PROJECT_PATH%\settings.ini" "%PROJECT_PATH%\dist\settings.ini"/y
 
 if %errorlevel% neq 0 (
     echo An error occured while creating executable .exe-file, setup aborted!
