8a2e0d31d8863a69fccbd6e4ce2f2a56122bc278
Pielau92 <106060352+Pielau92@users.noreply.github.com>
2025-02-24 14:13:22 +0100

    Fix errors in build_executable
    - pyinstaller was commented out
    - executable copying process had an error

--- scripts/build_executable.bat
+++ scripts/build_executable.bat
@@ -8,7 +8,7 @@ rem Change directory
 cd %PROJECT_PATH%
 
 echo Creating .exe file with PyInstaller...
-rem %PYINSTALLER_PATH% %MAIN_PATH% --clean --onefile --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/DLLs/pyexpat.pyd;dlls" --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/libexpat.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/libcrypto-3-x64.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/libssl-3-x64.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/liblzma.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/LIBBZ2.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/sqlite3.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/tk86t.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/tcl86t.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/ffi.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/libmpdec-4.dll;."
+%PYINSTALLER_PATH% %MAIN_PATH% --clean --onefile --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/DLLs/pyexpat.pyd;dlls" --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/libexpat.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/libcrypto-3-x64.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/libssl-3-x64.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/liblzma.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/LIBBZ2.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/sqlite3.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/tk86t.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/tcl86t.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/ffi.dll;." --add-binary "C:/Users/pierre/miniconda3/envs/TRNSYSAuto/Library/bin/libmpdec-4.dll;."
 
 rem Check for error
 if %errorlevel% neq 0 (
@@ -19,7 +19,7 @@ if %errorlevel% neq 0 (
 rem --collect-all xml.parsers.expat --hidden-import=pyexpat --collect-all pyexpat --hidden-import=openpyxl.cell._writer --hidden-import=win32timezone --hidden-import=tkinter 
 
 echo Copying executable into %PROJECT_PATH%
-xcopy "PROJECT_PATH\dist\main.exe" . /y || (exit /b %errorlevel%)
+xcopy %PROJECT_PATH%\dist\main.exe %PROJECT_PATH% /y || (exit /b %errorlevel%)
 
 echo Executable created successfully!
 endlocal
