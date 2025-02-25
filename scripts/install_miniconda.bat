ea1a9bd80f05dca2f23309e5659d881e77c74151
Pielau92 <106060352+Pielau92@users.noreply.github.com>
2025-02-25 10:47:08 +0100

    Add miniconda installation script
    Added script, which installs the most recent miniconda version directly from the internet.

---
+++ scripts/install_miniconda.bat
@@ -0,0 +1,8 @@
+@echo off
+setlocal
+
+echo Installing miniconda
+curl https://repo.anaconda.com/miniconda/Miniconda3-latest-Windows-x86_64.exe --output .\Downloads\Miniconda3-latest-Windows-x86_64.exe
+
+endlocal
+pause
