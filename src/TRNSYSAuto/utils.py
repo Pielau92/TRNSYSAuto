45cc59bc043a2d2caee511255360741a6fd4188e
Pielau92 <106060352+Pielau92@users.noreply.github.com>
2026-03-30 17:10:36 +0200

    Move utils to trnsys_simulation
    Moved all utils function needed for single simulations into trnsys_simulation directory.
    
    All utils in trnsys_simulation are also forwarded to TRNSYSAuto.utils.py.

--- src/TRNSYSAuto/utils.py
+++ src/TRNSYSAuto/utils.py
@@ -1,5 +1,4 @@
 import sys, os
-import re
 import shutil
 import win32com.client
 
@@ -10,6 +9,8 @@ from tkinter import filedialog  # explicit import required, as calling from tk.f
 from pandas import Series, DataFrame
 from openpyxl import load_workbook
 from openpyxl.utils.cell import coordinate_from_string, column_index_from_string
+
+from trnsys_simulation.utils import *
 
 
 def get_root_dir() -> str:
@@ -22,101 +23,6 @@ def get_root_dir() -> str:
         return parent_dir(path=sys.executable, levels=2)
     else:  # if program is run from IDE or command window
         return parent_dir(path=__file__, levels=3)
-
-
-def parent_dir(path: str, levels: int = 1) -> str:
-    """Get parent directory.
-
-    :param str path: starting path
-    :param int levels: specifies how many directory levels to move upwards from the starting path
-    :return: parent directory path
-    """
-
-    for _ in range(levels):
-        path = os.path.dirname(path)
-    return path
-
-
-def replace_parameter_values(path_file: str, parameters: dict, mark: bool = False) -> None:
-    """Find and replace parameter values within a .txt file.
-
-    Finds parameter values within a .txt file, replaces them and overwrites the .txt file. For this, the following must
-    apply:
-    -   A parameter means in this case a name/value pair inside a txt file - specifically: a word followed by "=" and a
-        number (there may be any amount of white spaces or tabs between the word and "=" and between "=" and the number)
-    -   The replacement values have to be passed in "parameters", where the row indizes must match the name of the
-        parameter whose number value is to be replaced
-
-    Example:
-        # .txt file containing "Parameter1=1; Parameter2=2; @Parameter3=3."
-        path_file = 'C:\\Users\\JohnDoe\\Desktop\\text.txt'
-
-        parameters = pd.Series({'Parameter1': 4, 'Parameter2': 5, 'Parameter3': 6})
-
-        replace_parameter_value(path_file, parameters)
-
-        # .txt file now shows "Parameter1=4; Parameter2=5; Parameter3=6."
-
-    :param str path_file: path of txt file
-    :param dict parameters: dict with parameters as key value pairs
-    """
-
-    def replacer(match):
-        parameter = match.group(1)  # parameter name
-        initial_value = match.group(2).strip()
-        comment = match.group(3)  # miscellaneous characters after the number value (typically comments)
-
-        if comment is None:
-            comment = ''
-
-        if parameter in parameters.keys():
-            new_value = str(parameters[parameter])
-        else:
-            return match.group(0)  # return unchanged text
-
-        if initial_value == new_value:
-            return match.group(0)  # return unchanged text
-
-        if mark:  # mark changed lines with comment
-            comment += f' ! *parameter changed from {initial_value} to {new_value}*'  # add remark
-
-        return f"{parameter} = {new_value} {comment}"  # replace, if parameter name matches
-
-    with open(path_file, 'r') as file:
-        text = file.read()
-
-    pattern = re.compile(
-        r'^(\w*)'  # word at beginning of line
-        + r'[\s\t]*=[\s\t]*'  # equal sign (=), with any number of white spaces/tabs before and after
-        + r'([^\n!]*)'  # any number of characters, that are neither an exclamation mark nor the end of the line
-        + r'(?:(!.*))?'  # (optional) exclamation mark followed by any number of characters
-        + r'$',  # end of line
-        re.MULTILINE
-    )
-
-    new_text = pattern.sub(replacer, text)
-
-    # overwrite file
-    with open(path_file, 'w') as file:
-        file.write(new_text)
-
-
-def find_and_replace(path_file: str, pattern: str, replacement: str) -> None:
-    """Find string in text and replace by another string.
-
-    Reads text from a .txt file, searches for a defined string, replaces it by another string and overwrites the .txt
-    file.
-
-    :param str path_file: path to txt file
-    :param str pattern: pattern which marks the text to be replaced
-    :param str replacement: text to be inserted instead
-    """
-
-    with open(path_file, 'r') as file:
-        text = file.read()  # read file
-        new_text = re.sub(pattern=pattern, repl=replacement, string=text)
-    with open(path_file, 'w') as file:
-        file.write(new_text)  # overwrite file
 
 
 def update_excel_file(path_excel_file: str) -> None:
@@ -316,19 +222,6 @@ def logical_or(boolean_lists: list[list[bool]]) -> list[bool]:
         raise ValueError('All boolean lists must have the same length.')
 
     return [any(values) for values in zip(*boolean_lists)]
-
-
-def delete_files(paths: list[str]) -> None:
-    """Delete multiple files.
-
-    :param paths: list of paths to files to be deleted
-    """
-
-    for path in paths:
-        try:
-            os.remove(path)
-        except FileNotFoundError:
-            pass
 
 
 def cell_insert_series_to_excel(data: pd.Series, path: str, sheet_name: str, start_cell: str):
